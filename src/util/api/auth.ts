a51831da9d84786d0c05376ba5c8d47eb2054ec0
DELTA\ace.ca.chiu <ace.ca.chiu@deltaww.com>
2023-08-03 09:13:13 +0800

    prettier format done

--- src/util/api/auth.ts
+++ src/util/api/auth.ts
@@ -1,7 +1,7 @@
 import { authAxiosInstance } from '../axiosInstance'
-const url = "/auth/current-user/"
+const url = '/auth/current-user/'
 /// 取得 Current User
 export const getCurrentUser = () => {
-    console.log(authAxiosInstance)
-    return authAxiosInstance.get(`${url}`)
+  console.log(authAxiosInstance)
+  return authAxiosInstance.get(`${url}`)
 }
