28975a63ce1ef07ce8f2f6b66cd9ad4f01ba52a9
virgineyes <vulmp999@gmail.com>
2023-11-06 18:26:41 +0800

    0.0.1

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,17 +5,17 @@ import {
   Router,
   RouteRecordRaw
 } from 'vue-router'
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
 import pinia from '../store/store'
 import { base } from '../store/base'
-import { getCurrentUser } from '../util/api/auth'
-import { ElMessage } from 'element-plus'
+// import { getCurrentUser } from '../util/api/auth'
+// import { ElMessage } from 'element-plus'
 import scrollToTop from '~/util/composables/basic'
-import { useRouter, useRoute } from 'vue-router'
+// import { useRouter, useRoute } from 'vue-router'
 
 const baseI = base(pinia)
-const $router = useRouter()
-const $route = useRoute()
+// const $router = useRouter()
+// const $route = useRoute()
 const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/home' },
   { path: '/home', name: 'home', component: () => import('../views/Home.vue') },
@@ -51,86 +51,87 @@ const options: RouterOptions = {
 
 const router: Router = createRouter(options)
 
-const ValidateDeltaDomain = (url: string) => {
-  if (
-    process.env.NODE_ENV.trim() === 'development' ||
-    process.env.NODE_ENV.trim() === 'production'
-  ) {
-    return true
-  }
-  const validDomain = process.env.VUE_APP_DOMAIN
-  const host = new URL(url).hostname
-  return host === validDomain || host.endsWith('.' + validDomain)
-}
+// const ValidateDeltaDomain = (url: string) => {
+//   if (
+//     process.env.NODE_ENV.trim() === 'development' ||
+//     process.env.NODE_ENV.trim() === 'production'
+//   ) {
+//     return true
+//   }
+//   const validDomain = process.env.VUE_APP_DOMAIN
+//   const host = new URL(url).hostname
+//   return host === validDomain || host.endsWith('.' + validDomain)
+// }
 
 router.beforeEach((to, from, next) => {
-  baseI.loading = true
-  const token = to.query.token as string
-  if (token) {
-    Cookies.set(import.meta.env.VITE_APP_AUTH_TOKEN_NAME, token, {
-      expires: 1000 * 60 * 60 * 6
-    })
-    baseI.setLogin(true)
-    baseI.setRedirectUrl(to.path)
-    baseI.setToken(token)
-    $router.replace($route.path)
-  }
-  if (Cookies.get(import.meta.env.VITE_APP_AUTH_TOKEN_NAME)) {
-    if (Object.keys(baseI.getUser)?.length === 0) {
-      getCurrentUser()
-        .then((response: any) => {
-          baseI.setUser(response.data)
-        })
-        .catch((error: any) => {
-          Cookies.remove(import.meta.env.VITE_APP_AUTH_TOKEN_NAME)
-          ElMessage.error({
-            message: error,
-            duration: 5000
-          })
-        })
-        .finally(() => {
-          baseI.loading = false
-        })
-    }
-    next()
-  } else {
-    // Home 不需要登入(大寫區分 Redirect )
-    if (to.name === 'Error') {
-      next()
-    } else {
-      localStorage.setItem(
-        'loginAuthCount',
-        `${Number(localStorage.getItem('loginAuthCount') ?? 0) + 1}`
-      )
-      let authnURL = import.meta.env.VITE_APP_AUTH_URL
-      let frontURL = import.meta.env.VITE_APP_FRONTEND_URL
+  next()
+  // baseI.loading = true
+  // const token = to.query.token as string
+  // if (token) {
+  //   Cookies.set(import.meta.env.VITE_APP_AUTH_TOKEN_NAME, token, {
+  //     expires: 1000 * 60 * 60 * 6
+  //   })
+  //   baseI.setLogin(true)
+  //   baseI.setRedirectUrl(to.path)
+  //   baseI.setToken(token)
+  //   // $router.replace($route.path)
+  // }
+  // if (Cookies.get(import.meta.env.VITE_APP_AUTH_TOKEN_NAME)) {
+  //   if (Object.keys(baseI.getUser)?.length === 0) {
+  //     getCurrentUser()
+  //       .then((response: any) => {
+  //         baseI.setUser(response.data)
+  //       })
+  //       .catch((error: any) => {
+  //         Cookies.remove(import.meta.env.VITE_APP_AUTH_TOKEN_NAME)
+  //         ElMessage.error({
+  //           message: error,
+  //           duration: 5000
+  //         })
+  //       })
+  //       .finally(() => {
+  //         baseI.loading = false
+  //       })
+  //   }
+  //   next()
+  // } else {
+  //   // Home 不需要登入(大寫區分 Redirect )
+  //   if (to.name === 'Error') {
+  //     next()
+  //   } else {
+  //     localStorage.setItem(
+  //       'loginAuthCount',
+  //       `${Number(localStorage.getItem('loginAuthCount') ?? 0) + 1}`
+  //     )
+  //     let authnURL = import.meta.env.VITE_APP_AUTH_URL
+  //     let frontURL = import.meta.env.VITE_APP_FRONTEND_URL
 
-      if (!ValidateDeltaDomain(authnURL)) {
-        authnURL = ''
-        next({ name: 'Error', query: { errorCode: '403', redirect: 'false' } })
-      }
-      if (!ValidateDeltaDomain(frontURL)) {
-        frontURL = ''
-        next({ name: 'Error', query: { errorCode: '403', redirect: 'false' } })
-      }
+  //     if (!ValidateDeltaDomain(authnURL)) {
+  //       authnURL = ''
+  //       next({ name: 'Error', query: { errorCode: '403', redirect: 'false' } })
+  //     }
+  //     if (!ValidateDeltaDomain(frontURL)) {
+  //       frontURL = ''
+  //       next({ name: 'Error', query: { errorCode: '403', redirect: 'false' } })
+  //     }
 
-      if (Number(localStorage.getItem('loginAuthCount')) < 3) {
-        window.location.href =
-          `${authnURL}api/auth/redirect?redirectUrl=` +
-          encodeURIComponent(`${frontURL}#${to.path}`) +
-          '&errorUrl=' +
-          `${frontURL}#/home`
-      } else {
-        localStorage.setItem('loginAuthCount', '0')
-        window.location.href =
-          `${authnURL}#/auth/login?redirectUrl=` +
-          encodeURIComponent(`${frontURL}#${to.path}`) +
-          '&errorUrl=' +
-          `${frontURL}#/home`
-      }
-    }
-    baseI.loading = false
-  }
+  //     if (Number(localStorage.getItem('loginAuthCount')) < 3) {
+  //       window.location.href =
+  //         `${authnURL}api/auth/redirect?redirectUrl=` +
+  //         encodeURIComponent(`${frontURL}#${to.path}`) +
+  //         '&errorUrl=' +
+  //         `${frontURL}#/home`
+  //     } else {
+  //       localStorage.setItem('loginAuthCount', '0')
+  //       window.location.href =
+  //         `${authnURL}#/auth/login?redirectUrl=` +
+  //         encodeURIComponent(`${frontURL}#${to.path}`) +
+  //         '&errorUrl=' +
+  //         `${frontURL}#/home`
+  //     }
+  //   }
+  //   baseI.loading = false
+  // }
 })
 router.afterEach(() => {
   baseI.loading = false
