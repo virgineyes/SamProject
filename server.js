e20c147bf773deb21b3079687f642955fed8f2ed
virgineyes <vulmp999@gmail.com>
2023-11-06 18:37:08 +0800

    install express

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var serveStatic = require('serve-static');
 var path = require('path');
 
-app = express();
+var app = express();
 
 app.use('/',serveStatic(path.join(__dirname, '/dist')));
 var port = process.env.PORT || 8080;
